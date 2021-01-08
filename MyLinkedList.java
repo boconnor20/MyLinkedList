a2b53b4ea251414655df0b51020e1b5eeead3f13
Brandon Oconnor <boconnor20@stuy.edu>
2021-01-08 00:37:00 -0500

    created add methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -10,10 +10,38 @@ public class MyLinkedList{
  public int size(){
    return size;
  }
- public boolean add(String value);
- public boolean add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+ public boolean add(String value){
+   Node  newNode=new Node(value);
+   newNode.setNext(null);
+   Node current=start;
+   while(current.getNext() != null){
+     current = current.getNext();
+   }
+   current.setNext(newNode);
+   size++;
+   return true;
+
+ }
+ public boolean add(int index, String value){
+   Node newNode = new Node(value);
+   Node current = start;
+   if(current != null){
+    for(int i = 0; i < index && current.getNext() != null; i++){
+      current = current.getNext();
+    }
+   }
+   newNode.setNext(current.getNext());
+   current.setNext(newNode);
+   size ++;
+  }
+ public String get(int index){
+
+ }
+ public String set(int index, String value){
+
+ }
+ public String toString(){
+
+ }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
