22826f7bd5237a2d9410c5bf1e24c28462d65700
Brandon Oconnor <boconnor20@stuy.edu>
2021-01-08 00:24:30 -0500

    created initial methods for Node

--- Node.java
+++ Node.java
@@ -3,4 +3,24 @@ public class Node{
  private String data;
  private Node next,prev;
  //write get/set methods for all three instance variables.
-}
+ public String getData(){
+    return data;
+  }
+  public Node getNext(){
+    return next;
+  }
+  public Node getPrev(){
+    return prev;
+  }
+
+  public void setData(String data){
+    this.data = data;
+  }
+  public void setNext(Node next){
+    this.next = next;
+  }
+  public void setPrev(Node prev){
+    this.prev = prev;
+  }
+
+ }
